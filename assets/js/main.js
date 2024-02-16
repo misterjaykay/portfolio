a2b9d238b52ae8a31011272df0f2d5099ae8e485
telegraph-chris <ck@telegraphbranding.com>
2024-02-16 15:33:02 -0600

    portfolio update.

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,7 +24,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
     }
   }
 
-  typeWriter();
+  // typeWriter();
 
   // function buttonAnimate() {
   //   var id = null;
@@ -81,3 +81,20 @@ window.onclick = function(event) {
 
 });
 
+$(document).ready(function(){
+  $(window).scroll(function(){
+      // sticky navbar on scroll script
+      if(this.scrollY > 20){
+          $('.navbar').addClass("sticky");
+      }else{
+          $('.navbar').removeClass("sticky");
+      }
+      
+      // scroll-up button show/hide script
+      if(this.scrollY > 500){
+          $('.scroll-up-btn').addClass("show");
+      }else{
+          $('.scroll-up-btn').removeClass("show");
+      }
+  });
+});
